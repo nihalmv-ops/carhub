47c8ffd452af28fbee668a27f0a86f6a288f5e79
nihalmv-ops <nihalmv86@gmail.com>
2026-06-04 19:21:49 +0530

    Add premium navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        CarHub
+      </div>
+
+      <ul className="menu">
+        <li>Home</li>
+        <li>Cars</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
