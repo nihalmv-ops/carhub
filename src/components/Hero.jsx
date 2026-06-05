702b331327998508211f2b1aae4cd908741df9a0
nihalmv-ops <nihalmv86@gmail.com>
2026-06-05 19:06:20 +0530

    Add premium hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+export default function Hero(){
+    return(
+        <section className="hero">
+            <h1>
+                DRIVE YOUR DREAM CAR
+            </h1>
+            <p>
+                Discover luxury performance and timeless desing.
+            </p>
+            <button>
+                Explore cars
+            </button>
+        </section>
+    )
+}
